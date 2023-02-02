ac6345808b186d68a31f1fc4cbde6466f067cfad
dharana <dharana7723@gmail.com>
2023-02-02 18:23:21 +0900

    feat: basic 구조 추가

---
+++ src/test/java/kr/co/pro/order/OrderApplicationTests.java
@@ -0,0 +1,12 @@
+package kr.co.programmers.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+}
